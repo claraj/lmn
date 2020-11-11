afb086f169cab3dff8e009b1f031a5a203c103b9
KJTempas <fw6927mm@go.minneapolis.edu>
2020-11-11 15:12:48 -0600

    fix view folder name in urls.py

--- lmn/urls.py
+++ lmn/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.contrib.auth import views as auth_views
 
 from . import views
-from .views import views_main, views_artists, views_venues, views_notes, views_users, admin_views
+from .views import views_main, views_artists, views_venues, views_notes, views_users, views_admin
 
 
 # app_name = 'lmn'
