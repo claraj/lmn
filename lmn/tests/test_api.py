1f1596f9793a7205c8c7918d6233bf2b4c8e3b04
KJTempas <fw6927mm@go.minneapolis.edu>
2020-11-17 08:57:22 -0600

    add 2 tests of api info to dbase

--- lmn/tests/test_api.py
+++ lmn/tests/test_api.py
@@ -1,6 +1,6 @@
 
-import unittest
-from unittest import TestCase
+#import unittest
+#from unittest import TestCase
 from lmn.views import views_admin
 
 #imports below copied from test_views.py
@@ -21,7 +21,7 @@ from datetime import timezone
 
 class TestTicketMasterAPI(TestCase):
 
-    def test_retrieve_data(self):
+    def setUp(self):
         example_api_response = {
         "_embedded": {
             "events": [
@@ -55,21 +55,71 @@ class TestTicketMasterAPI(TestCase):
                         }]}
                 }]}}
 
-        # expected_result = [
-        #     {"performer": "George Strait",
-        #     "venueName": "U.S Bank Stadium",
-        #      "venueCity": "Minneapolis",
-        #      "venueState": "MN",
-        #      "show_date_time": "2021-07-31T22:00:00Z"
-        #     }
-        # ]
-
         #call method in views_admin,feeding the example response
-        views_admin.get_music_data(example_api_response)
-        #method returns 200 if successful. 
-        #  
-        #data saved to test dbase? - need a dummy dbase?
-        #then make sure artist.name="George Strait"
-            #venue.name = .... etc?
+        #this should save data to the database
+        views_admin.extract_music_details(example_api_response)
+    
+        
+        #was data saved to dbase?  
+    def test_save_artist_name_in_artist_table(self):
+        response = self.client.get( reverse('artist_list') , {'search_name' : 'George Strait'} )
+        self.assertContains(response, 'George Strait')
+        self.assertNotContains(response,'Queen' )
 
+
+    def test_venue_saved(self):
+        response = self.client.get( reverse('venue_list') , {'search_name' : 'U.S. Bank Stadium'} )
+        self.assertContains(response, 'U.S. Bank Stadium')
+        self.assertContains(response, 'MN')
+        self.assertContains(response, 'Minneapolis')
+        self.assertNotContains(response, 'The Lounge')
+        self.assertNotContains(response, 'Kansas City')
+        self.assertNotContains(response, 'KS')
+
+    #def test_show_date_saved(self):
+
+    # to test unique together constraint- so can't add duplicate show
+    # def test_unique_together(self):
+    #     example_api_response = {
+    #     "_embedded": {
+    #         "events": [
+    #             {
+    #             "name": "George Strait",
+    #             "type": "event",
+    #             "url": "https://www.ticketmaster.com/george-strait-minneapolis-minnesota-07-31-2021/event/06005830D6D4864F",
+    #             "dates": {
+    #                 "start": {
+    #                     "localDate": "2021-07-31",
+    #                     "localTime": "17:00:00",
+    #                     "dateTime": "2021-07-31T22:00:00Z",
+    #                 },
+    #             "timezone": "America/Chicago",
+    #             },
+                
+    #             "_embedded": {
+    #                 "venues": [
+    #                     {
+    #                     "name": "U.S. Bank Stadium",
+    #                     "type": "venue",
+    #                     "postalCode": "55415",
+    #                     "timezone": "America/Chicago",
+    #                     "city": {
+    #                         "name": "Minneapolis"
+    #                     },
+    #                     "state": {
+    #                         "name": "Minnesota",
+    #                         "stateCode": "MN",
+    #                     }
+    #                     }]}
+    #             }]}}
+
+    #     #call method in views_admin,feeding the example response
+    #     #this should save data to the database
+    #     views_admin.extract_music_details(example_api_response)
+
+    #     response = self.client.get(reverse('artist_list'))
+    #     self.assertEqual(len(response.context['artists']), 1)
+
+
+    
         
--- lmn/views/views_admin.py
+++ lmn/views/views_admin.py
@@ -19,34 +19,38 @@ def get_music_data(request):
         response = requests.get(url, params=query)
         response.raise_for_status()  #will raise an exception for 400(client) or 500(server) errors
         data = response.json() 
-        events = data['_embedded']['events']
-        
-        for event in events: 
-            pattern = '[A-Za-z\s]{1,30}' 
-            performerLong = event['name']
-            performerObj = re.search(pattern, performerLong)
-            performer = performerObj.group()
-            venueName = event['_embedded']['venues'][0]['name']
-            venueCity = event['_embedded']['venues'][0]['city']['name']
-            venueState = event['_embedded']['venues'][0]['state']['stateCode']
-            show_date_time = event['dates']['start']['dateTime']   
-         
-           ##linking info to models and saving it
-            new_artist = Artist(name=performer)
-            new_artist.save() #must save the new artist, then get id
-            new_artist.id
-            
-            new_venue = Venue(name=venueName, city=venueCity, state=venueState)
-            new_venue.save()
-            new_venue.id
-
-            new_show=Show(show_date=show_date_time, artist_id = new_artist.id, venue_id = new_venue.id)
-            new_show.save()
-        return HttpResponse('ok')
-        
+        extract_music_details(data)
     except Exception as ex:
         print(ex)
        # logging.exception(ex)
+        
+def extract_music_details(data):
+    events = data['_embedded']['events']
+    
+    for event in events: 
+        pattern = '[A-Za-z\s]{1,30}' 
+        performerLong = event['name']
+        performerObj = re.search(pattern, performerLong)
+        performer = performerObj.group()
+        venueName = event['_embedded']['venues'][0]['name']
+        venueCity = event['_embedded']['venues'][0]['city']['name']
+        venueState = event['_embedded']['venues'][0]['state']['stateCode']
+        show_date_time = event['dates']['start']['dateTime']   
+        
+        ##linking info to models and saving it
+        new_artist = Artist(name=performer)
+        new_artist.save() #must save the new artist, then get id
+        new_artist.id
+        
+        new_venue = Venue(name=venueName, city=venueCity, state=venueState)
+        new_venue.save()
+        new_venue.id
+
+        new_show=Show(show_date=show_date_time, artist_id = new_artist.id, venue_id = new_venue.id)
+        new_show.save()
+    return HttpResponse('ok')
+        
+    
 
 
 
