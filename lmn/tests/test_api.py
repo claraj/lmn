db2d7a7323c773bcea623a4f001865d07d5a748a
KJTempas <fw6927mm@go.minneapolis.edu>
2020-11-21 14:13:47 -0600

    work on testing

--- lmn/tests/test_api.py
+++ lmn/tests/test_api.py
@@ -1,106 +1,78 @@
+import requests
+import unittest
+#?? I thought I had to use django.test import TestCase??
+from unittest import TestCase
+from unittest import mock
+from ..models import Artist, Venue, Show
+
+from lmn.views.views_admin import get_music_data, extract_music_details
 
 
-from lmn.views import views_admin
+def mock_requests_get(url, params):
+    class MockAPI:
+        def __init__(self, json_data):
+            self.json_data = json_data
 
-#imports below copied from test_views.py
-#maybe don't need all...TBD
-from django.test import TestCase, Client
-
-from django.urls import reverse
-from django.contrib import auth
-from django.contrib.auth import authenticate
-
-from lmn.models import Venue, Artist, Note, Show
-from django.contrib.auth.models import User
-
-import re, datetime
-from datetime import timezone
-
-
-
-class TestTicketMasterAPI(TestCase):
-
-    def setUp(self):
-        example_api_response = {
-        "_embedded": {
-            "events": [
-                {
-                "name": "George Strait",
-                "type": "event",
-                "url": "https://www.ticketmaster.com/george-strait-minneapolis-minnesota-07-31-2021/event/06005830D6D4864F",
-                "dates": {
-                    "start": {
-                        "localDate": "2021-07-31",
-                        "localTime": "17:00:00",
-                        "dateTime": "2021-07-31T22:00:00Z",
-                    },
-                "timezone": "America/Chicago",
-                },
-                "_embedded": {
-                    "venues": [
-                        {
-                        "name": "U.S. Bank Stadium",
-                        "postalCode": "55415",
-                        "timezone": "America/Chicago",
-                        "city": {
-                            "name": "Minneapolis"
-                        },
-                        "state": {
-                            "name": "Minnesota",
-                            "stateCode": "MN",
-                        }
-                        }]}
-                }]}}
-
-        #call method in views_admin,feeding the example response
-        #this should save data to the database
-        #views_admin.extract_music_details(example_api_response)
-        views_admin.get_music_data(example_api_response)
+        def json(self):
+            return self.json_data
+#query= {'classificationName': classificationName, 'city' : city, 'apikey': key}
         
-        #was data saved to dbase?  
-    def test_save_artist_name_in_artist_table(self):
-        response = self.client.get( reverse('artist_list') , {'search_name' : 'George Strait'} )
-        self.assertContains(response, 'George Strait')
-        self.assertNotContains(response,'Queen' )
+    if params['classificationName'] == 'music' and params['city'] == 'Minneapolis':
+        return MockAPI({
+        "_embedded": {"events": [{"name": "George Strait", "dates": { "start": {"dateTime": "2021-07-31T22:00:00Z"}, },
+        "_embedded": {"venues": [{"name": "U.S. Bank Stadium","city": { "name": "Minneapolis"},"state": {"stateCode": "MN", } }]}}]}})
+        #try again
+        #return MockAPI({"_embedded": {"events": [{"name": "George Strait","dates": {"start": {"dateTime": "2021-07-31T22:00:00Z",},},
+         #   "_embedded": {"venues": [{"name": "U.S. Bank Stadium","city": {"name": "Minneapolis"},"state": {"stateCode": "MN"},}],}}
 
+    return MockAPI(None)  #???
 
-    def test_no_duplicate_artist(self):
-        #set up loads artist George Strait
-        initial_artist_count = Artist.objects.count()
-        print(initial_artist_count)
-        performer = 'George Strait'
-        artist = Artist(name = performer)
-        artist.save() #try to save artist, but since unique constraint, will not save
-        self.assertEqual(Artist.objects.count(), initial_artist_count)
+class TestTickemasterAPI(TestCase):
+        #when patching, return side_effect instead of real request.get from API
+    @mock.patch('requests.get', side_effect=mock_requests_get)
+    def test_fetch(self, mock_get):
+            #assert requests.get call returns this response
+        mock_response = {
+    "_embedded": {"events": [{"name": "George Strait", "dates": { "start": {"dateTime": "2021-07-31T22:00:00Z"}, },
+    "_embedded": {"venues": [{"name": "U.S. Bank Stadium","city": { "name": "Minneapolis"},"state": {"stateCode": "MN", } }] }} ]}}
+        params = ('music', 'minneapolis')
+        error, data =get_music_data( params)
+        self.assertIsNone(error)
+        self.assertEqual(mock_response, data)
 
-
-    def test_venue_saved(self):
-            response = self.client.get( reverse('venue_list') , {'search_name' : 'U.S. Bank Stadium'} )
-            self.assertContains(response, 'U.S. Bank Stadium')
-            self.assertContains(response, 'MN')
-            self.assertContains(response, 'Minneapolis')
-            self.assertNotContains(response, 'The Lounge')
-            
-
-    def test_no_duplicate_venue(self):
-        initial_venue_count= Venue.objects.count()
-        venue = Venue(name = 'U.S. Bank Stadium', city='Minneapolis', state = 'MN').save()
-        current_venue_count = Venue.objects.count()
-        #counts should be the same as venue is a duplicate and should not be added
-        self.assertEqual(initial_venue_count,current_venue_count)
-
-
-    def test_save_show(self):
-       # self.fail()
-       pass
-
-
-    def test_no_duplicate_show(self):
+#was data saved to dbase? 
+    @mock.patch('requests.get', side_effect=mock_requests_get)
+    def test_save_artist_name_in_artist_table(self, mock_get):
+        mock_response = {"_embedded": {"events": [{"name": "George Strait", "dates": { "start": {"dateTime": "2021-07-31T22:00:00Z"}, },
+    "_embedded": {"venues": [{"name": "U.S. Bank Stadium","city": { "name": "Minneapolis"},"state": {"stateCode": "MN", } }] }} ]}}
+        #artist_name = 'George Strait'
+        #details = extract_music_details(mock_response)
+        extract_music_details(mock_response) #should save info to dbase
+        #below can only be used with django test case
+        #response = self.client.get(reverse('artist_list') , {'search_name' : 'George Strait'} )
         
-        initial_show_count= Show.objects.count()
-        print('showCount', initial_show_count)
-        #try to add a duplicate show
-        show = Show(show_date='2021-07-31 22:00:00' , artist ='George Strait' ,venue = 'U.S. Bank Stadium').save()
-        new_show_count = Show.objects.count()
-        #show should not add to dbase because not unique, so count stays same
-        self.assertEqual(initial_show_count, new_show_count)
+        artist_1 = Artist.objects.get(pk=1)
+        self.assertEqual( artist_1.name, 'George Strait')
+        
+        
+
+
+    # def test_retrieve_artist(self):
+    #     mock_response = {
+    #     "_embedded": {"events": [{"name": "George Strait", "dates": { "start": {"dateTime": "2021-07-31T22:00:00Z"}, },
+    #     "_embedded": {"venues": [{"name": "U.S. Bank Stadium","city": { "name": "Minneapolis"},"state": {"stateCode": "MN", } }]}}]}}
+    # #feed the mock response to the extract_music_details function
+    #     data = extract_music_details(mock_response)
+        
+    #     self.assertEqual('George Strait', data.artist.name)
+
+#was data saved to dbase? 
+        # def test_save_artist_name_in_artist_table(self):
+        #     response = self.client.get( reverse('artist_list') , {'search_name' : 'George Strait'} )
+        #     self.assertContains(response, 'George Strait')
+        #     self.assertNotContains(response,'Queen' )
+
+
+
+
+
--- lmn/views/views_admin.py
+++ lmn/views/views_admin.py
@@ -4,6 +4,7 @@ import re
 from ..models import Artist, Venue, Show
 from django.http import HttpResponse
 from django.http import Http404
+from django.db import IntegrityError
 
 #getting data from  ticketmaster api
 key = os.environ.get('TICKETMASTER_KEY')
@@ -11,19 +12,20 @@ url = 'https://app.ticketmaster.com/discovery/v2/events'
 classificationName = 'music'
 city = 'Minneapolis'
 
+#below is revised - breaking into two functions
 def get_music_data(request):
-   #data=get_music_data_from_ticketMaster(request)
-    #extract_music_details(data)
-
-#def get_music_data_from_ticketMaster(request):
+   
     try:
         query= {'classificationName': classificationName, 'city' : city, 'apikey': key}
-        response = requests.get(url, params=query)
-        response.raise_for_status()  #will raise an exception for 400(client) or 500(server) errors
-        data = response.json() 
-        #return data
-    
-#def extract_music_details(data):
+        data = requests.get(url, params=query).json()
+        return None, data
+    except ValueError as err:
+        logging.error(f'JSON response error: {err}')
+        return None, 'Ticketmaster did not return an appropriate JSON response'
+
+
+def extract_music_details(data):
+        #now need to extract relevant data from the resonse 
         events = data['_embedded']['events']
         
         for event in events: 
@@ -38,15 +40,31 @@ def get_music_data(request):
             show_date_time = event['dates']['start']['dateTime']   
             
             ##linking info to models and saving it
-            try: #if this artist already in database, don't add it again
-                artist = Artist.objects.get(name=performer)
-            except:# otherwise add a new artist 
+             #if this artist already in database, don't add it again
+            try:
+                if Artist.objects.get(name=performer):
+                    raise IntegrityError
+                else:
+                    artist = Artist(name=performer)
+                    artist.save() #must save the new artist, then get id
+                    artist.id
+
+            except :
+                raise IntegrityError as e# otherwise add a new artist 
+                log(e)
+
+
+
+            try:
+                artist =  Artist.objects.get(name=performer):
+                #raise IntegrityError
+            except :# otherwise add a new artist 
                 artist = Artist(name=performer)
                 artist.save() #must save the new artist, then get id
                 artist.id
-            try:
+            try:#if this venue already in the database, don't add it again
                 venue=Venue.objects.get(name=venueName)
-            except:
+            except: #if not already in dbase, create new Venue object and save it
                 venue = Venue(name=venueName, city=venueCity, state=venueState)
                 venue.save()
                 venue.id
@@ -57,9 +75,75 @@ def get_music_data(request):
                 show.save()
         return HttpResponse('ok')
             
-    except Exception as ex:
-        print(ex)
-       # logging.exception(ex)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+#this is original - before splitting into 2 functions
+#getting data from  ticketmaster api
+# key = os.environ.get('TICKETMASTER_KEY')
+# url = 'https://app.ticketmaster.com/discovery/v2/events'
+# classificationName = 'music'
+# city = 'Minneapolis'
+
+# def get_music_data(request):
+#    #data=get_music_data_from_ticketMaster(request)
+#     #extract_music_details(data)
+
+# #def get_music_data_from_ticketMaster(request):
+#     try:
+#         query= {'classificationName': classificationName, 'city' : city, 'apikey': key}
+#         response = requests.get(url, params=query)
+#         response.raise_for_status()  #will raise an exception for 400(client) or 500(server) errors
+#         data = response.json() 
+#         #return data
+    
+# #def extract_music_details(data):
+#         events = data['_embedded']['events']
+        
+#         for event in events: 
+#             pattern = '[A-Za-z\s]{1,30}' 
+#             performerLong = event['name']
+#             performerObj = re.search(pattern, performerLong)
+#             performer = performerObj.group()
+#             venueName = event['_embedded']['venues'][0]['name']
+#             venueCity = event['_embedded']['venues'][0]['city']['name']
+#             venueState = event['_embedded']['venues'][0]['state']['stateCode']
+            
+#             show_date_time = event['dates']['start']['dateTime']   
+            
+#             ##linking info to models and saving it
+#             try: #if this artist already in database, don't add it again
+#                 artist = Artist.objects.get(name=performer)
+#             except:# otherwise add a new artist 
+#                 artist = Artist(name=performer)
+#                 artist.save() #must save the new artist, then get id
+#                 artist.id
+#             try:
+#                 venue=Venue.objects.get(name=venueName)
+#             except:
+#                 venue = Venue(name=venueName, city=venueCity, state=venueState)
+#                 venue.save()
+#                 venue.id
+#             try: #if this show already in dbase, don't add it again
+#                 show = Show.objects.get(show_date=show_date_time, artist_id = artist.id, venue_id = venue.id)
+#             except: #otherwise, make a new Show object and save it
+#                 show=Show(show_date=show_date_time, artist_id = artist.id, venue_id = venue.id)
+#                 show.save()
+#         return HttpResponse('ok')
+            
+#     except Exception as ex:
+#         print(ex)
+#        # logging.exception(ex)
 
 
 
