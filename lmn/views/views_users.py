b24788fa61d9f412296b2bfeb2f4662d40adf18f
qian587 <tanxx587@umn.edu>
2020-11-24 21:23:53 -0500

    clear form after submit and added some styling for message

--- lmn/views/views_users.py
+++ lmn/views/views_users.py
@@ -18,48 +18,32 @@ def user_profile(request, user_pk):
 
 @login_required
 def my_user_profile(request):
-    # TODO - editable version for logged-in user to edit their own profile
     if request.method == 'POST':
         profile = Profile.objects.get(user=request.user)
         form = UserProfileForm(request.POST, instance=profile) 
+
+        context = {
+        'user_form' : form,
+        'user_profile': request.user
+        }
         if form.is_valid():
             form.save()
             messages.success(request, 'Your profile has been updated!')
-            
+            new_user_form = UserProfileForm(instance=request.user.profile)
+            return redirect('my_user_profile')
         else:
             messages.error(request, form.errors)
+            return render(request, 'lmn/users/profile.html')
+            
         return redirect('my_user_profile')
     else:
-        # profile = Profile.objects.get(user=request.user)
-        # TODO: handle profile that does not exist yet
-        user_form = UserProfileForm(instance=request.user.profile)
+        user_form = UserProfileForm()
     context = {
         'user_form' : user_form,
         'user_profile': request.user
     }
     
-    # return redirect('user_profile', user_pk=request.user.pk)
     return render(request, 'lmn/users/profile.html', context)
-
-
-# def update_profile(request):
-#     if request.method == 'POST':
-#         user_form = UserForm(request.POST, instance=request.user)
-#         profile_form = ProfileForm(request.POST, instance=request.user.profile)
-#         if user_form.is_valid() and profile_form.is_valid():
-#             user_form.save()
-#             profile_form.save()
-#             messages.success(request, _('Your profile was successfully updated!'))
-#             return redirect('settings:profile')
-#         else:
-#             messages.error(request, _('Please correct the error below.'))
-#     else:
-#         user_form = UserForm(instance=request.user)
-#         profile_form = ProfileForm(instance=request.user.profile)
-#     return render(request, 'profiles/profile.html', {
-#         'user_form': user_form,
-#         'profile_form': profile_form
-#     })
 
 
 def register(request):
