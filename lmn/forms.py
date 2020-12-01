6edf3a94bd2ac3770ccdd8d465cb48a4e59bdf70
name <me@gmail.com>
2020-11-30 19:53:58 -0600

    Added functionality for user to search notes by username

--- lmn/forms.py
+++ lmn/forms.py
@@ -14,6 +14,10 @@ class ArtistSearchForm(forms.Form):
     search_name = forms.CharField(label='Artist Name', max_length=200)
 
 
+class NoteSearchForm(forms.Form):
+    search_name = forms.CharField(label='Username', max_length=200)
+
+
 class NewNoteForm(forms.ModelForm):
     class Meta:
         model = Note
