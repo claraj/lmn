c8f218a931c1dddce8e2a0d139a2355e981e98b0
qian587 <tanxx587@umn.edu>
2020-12-08 16:13:15 -0500

    added breadcrumbs to links bar and added top shows page

--- lmn/views/views_notes.py
+++ lmn/views/views_notes.py
@@ -44,3 +44,7 @@ def notes_for_show(request, show_pk):
 def note_detail(request, note_pk):
     note = get_object_or_404(Note, pk=note_pk)
     return render(request, 'lmn/notes/note_detail.html' , { 'note': note })
+
+def top_shows(request):
+    notes = Note.objects.all().order_by('-rating')
+    return render(request, 'lmn/top_shows.html', { 'notes': notes })
