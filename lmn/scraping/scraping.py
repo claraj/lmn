59016dd3525e3a61a0ec88a650ced33b49bc96f5
John Cokins <il7148pg@go.minnstate.edu>
2021-04-18 08:42:13 -0500

    beautiful soup selector working

--- lmn/scraping/scraping.py
+++ lmn/scraping/scraping.py
@@ -7,10 +7,13 @@ def scrape_first():
     try:
         r = requests.get(url)
         soup = BeautifulSoup(r.content, 'html.parser')
-        print(soup)    
     except Exception as e:
         print(e)
     
-
+# selecting elements with <div class="d-flex flex-column h-100 flex-fill">
+    seive = soup.select('.h-100')
+    print(seive)
+    # for item in seive:
+    #     print(item)
 
 scrape_first()
