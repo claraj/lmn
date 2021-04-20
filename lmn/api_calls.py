08d02b010076770b584d059c1d79595067d35f8f
mania138 <123@fakestreet.com>
2021-04-19 22:43:07 -0500

    added eception handling for connection error with API, now will return None if no results (need to prepare for that when we call them 'if results:'

--- lmn/api_calls.py
+++ lmn/api_calls.py
@@ -11,16 +11,20 @@ def search_mb_place(place):
         'fmt=': 'json',
         'limit': 10
     }
-    data = requests.get(url, headers=headers, params=params).json()
-    places = data['places']
-    result_list = []
-    for place in places:
-        new_name = place['name']
+    try:
+        data = requests.get(url, headers=headers, params=params).json()
+        places = data['places']
+        result_list = []
+        for place in places:
+            new_name = place['name']
 
-        new_address = place.get('address')
-        new_venue = Venue(new_name, new_address)
-        result_list.append(new_venue)
-    return result_list
+            new_address = place.get('address')
+            new_venue = Venue(new_name, new_address)
+            result_list.append(new_venue)
+        return result_list
+    except requests.ConnectionError as e:
+        print(f'Error with connection {e}')
+        return None
 
 
 def search_mb_artist_by_name(name):
@@ -32,25 +36,29 @@ def search_mb_artist_by_name(name):
         'fmt': 'json',
         'limit': 10
     }
-    data = requests.get(url, headers=headers, params=params).json()
-    artists = data['artists']
-    result_list = []
-    for artist in artists:
-        new_name = artist['name']
-        try:
-            new_hometown = artist['begin-area']['name']
-        except KeyError as e:
-            new_hometown = 'Unknown Origins'
-            print(f'Error, no hometown {e}')
-        try:
-            new_desc = artist['disambiguation']
-        except KeyError as e:
-            new_desc = 'None'
-            print(f'Error, no description available {e}')
+    try:
+        data = requests.get(url, headers=headers, params=params).json()
+        artists = data['artists']
+        result_list = []
+        for artist in artists:
+            new_name = artist['name']
+            try:
+                new_hometown = artist['begin-area']['name']
+            except KeyError as e:
+                new_hometown = 'Unknown Origins'  # if no city of origin provided
+                print(f'Error, no hometown {e}')
+            try:
+                new_desc = artist['disambiguation']  # API description of artist
+            except KeyError as e:
+                new_desc = 'None'
+                print(f'Error, no description available {e}')
+            new_artist = Artist(new_name, new_hometown, new_desc)
+            result_list.append(new_artist)
+            for a in result_list:
+                if a.description == 'None':  # currently removing any Artists without description
+                    result_list.remove(a)
+            return result_list  # if successful, returns a list of artist objects
+    except requests.ConnectionError as e:  # if no response from API returns none
+        print(f'Error with connection {e}')
+    return None
 
-        new_artist = Artist(new_name, new_hometown, new_desc)
-        result_list.append(new_artist)
-    for a in result_list:
-        if a.description == 'None':
-            result_list.remove(a)
-    return result_list
