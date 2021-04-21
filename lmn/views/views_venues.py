2fba70ca6c7b977e77e1c4ae2ee07b210b24d9c7
PickleEric <60155510+PickleEric@users.noreply.github.com>
2021-04-21 13:06:45 -0500

    paginator added, not fully working.

--- lmn/views/views_venues.py
+++ lmn/views/views_venues.py
@@ -1,11 +1,11 @@
-from django.shortcuts import render, redirect, get_object_or_404
-
 from ..models import Venue, Artist, Note, Show
 from ..forms import VenueSearchForm, NewNoteForm, ArtistSearchForm, UserRegistrationForm
 
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.core.paginator import EmptyPage, Paginator
 
 
 def venue_list(request):
@@ -17,8 +17,19 @@ def venue_list(request):
         venues = Venue.objects.filter(name__icontains=search_name).order_by('name')
     else :
         venues = Venue.objects.all().order_by('name')   # Todo paginate
+        p = Paginator(venues, 2) # allows only 2 venues to be viewed per page
 
-    return render(request, 'lmn/venues/venue_list.html', { 'venues': venues, 'form': form, 'search_term': search_name })
+        print('Number of Pages:')
+        print(p.num_pages)
+
+        page_num = request.GET.get('page', 1)
+
+        try:
+            page = p.page(page_num)
+        except EmptyPage:
+            page = p.page(1) # shows page one rather then empty
+
+    return render(request, 'lmn/venues/venue_list.html', {  'venues': venues, 'form': form, 'search_term': search_name, 'page' : page})
 
 
 def artists_at_venue(request, venue_pk):   # pk = venue_pk
