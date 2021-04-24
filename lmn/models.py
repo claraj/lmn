819f6bb502b75f9e593f8a3d5e4ce53cacb36c47
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-04-23 21:19:51 -0500

    Add import statements

--- lmn/models.py
+++ lmn/models.py
@@ -3,6 +3,8 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Every model gets a primary key field by default.
 
