154a5fa5c8290bd3e49d9cce5111bc6342df27be
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-04-24 11:41:51 -0500

    Cascade deletion

--- lmn/models.py
+++ lmn/models.py
@@ -79,8 +79,8 @@ https://simpleisbetterthancomplex.com/tutorial/2016/07/22/how-to-extend-django-u
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User)
-    name = models.TextField(max_length=200, blank=False)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # name = models.TextField(max_length=200, blank=False)
     twitter_username = models.TextField(max_length=15, blank=True)  # Twitter usernames cannot be longer than 15 characters
     favorite_artist = models.ForeignKey(Artist, blank=True, on_delete=models.SET_NULL)
     favorite_show = models.ForeignKey(Show, blank=True, on_delete=models.SET_NULL)
