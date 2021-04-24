e0d975061586ad5369da1738c75a3dcababafef3
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-04-24 11:45:13 -0500

    Allow foreign keys to store null values

--- lmn/models.py
+++ lmn/models.py
@@ -83,8 +83,8 @@ class Profile(models.Model):
     # name = models.TextField(max_length=200, blank=False)
     twitter_username = models.CharField(max_length=15, blank=True)  # Twitter usernames cannot be longer than 15 characters
     bio = models.TextField(max_length=2000, blank=True)
-    favorite_artist = models.ForeignKey(Artist, blank=True, on_delete=models.SET_NULL)
-    favorite_show = models.ForeignKey(Show, blank=True, on_delete=models.SET_NULL)
+    favorite_artist = models.ForeignKey(Artist, blank=True, null=True, on_delete=models.SET_NULL)
+    favorite_show = models.ForeignKey(Show, blank=True, null=True, on_delete=models.SET_NULL)
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
