5b5b23f760408d374d4455c2cdbc3af93c41ef65
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-04-23 20:04:32 -0500

    Add favorite show to model

--- lmn/models.py
+++ lmn/models.py
@@ -76,3 +76,4 @@ class UserProfile(models.Model):
     name = models.TextField(max_length=200, blank=False)
     twitter_username = models.TextField(max_length=15, blank=True)  # Twitter usernames cannot be longer than 15 characters
     favorite_artist = models.ForeignKey(Artist, blank=True, on_delete=models.SET_NULL)
+    favorite_show = models.ForeignKey(Show, blank=True, on_delete=models.SET_NULL)
