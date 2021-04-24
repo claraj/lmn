31f9c0f7649a47e8198c4e0b210986de5ea73678
mania138 <123@fakestreet.com>
2021-04-24 17:34:01 -0500

    changed 'blank=true' in models

--- lmn/models.py
+++ lmn/models.py
@@ -22,9 +22,9 @@ User._meta.get_field('first_name')._blank = False
 
 class Artist(models.Model):
     """ updated model to match API call results """
-    name = models.CharField(max_length=200, blank=False)
-    hometown = models.CharField(max_length=200, blank=False)
-    description = models.CharField(max_length=200, blank=False)
+    name = models.CharField(max_length=200, blank=True)
+    hometown = models.CharField(max_length=200, blank=True)
+    description = models.CharField(max_length=200, blank=True)
 
     def __str__(self):
         artist_string = f'Artist: {self.name} From: {self.hometown} Description: {self.description}'
@@ -36,8 +36,8 @@ class Artist(models.Model):
 
 class Venue(models.Model):
     """ updated model to match API call results """
-    name = models.CharField(max_length=200, blank=False, unique=True)
-    address = models.CharField(max_length=400, blank=False, unique=True)
+    name = models.CharField(max_length=200, blank=True)
+    address = models.CharField(max_length=400, unique=True, blank=True)
 
     def __str__(self):
         return f'Name: {self.name} Location: {self.address}'
@@ -47,9 +47,9 @@ class Venue(models.Model):
 
 
 class Show(models.Model):
-    show_date = models.DateTimeField(blank=False)
-    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
-    venue = models.ForeignKey(Venue, on_delete=models.CASCADE)
+    show_date = models.DateTimeField(blank=True)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE, blank=True)
+    venue = models.ForeignKey(Venue, on_delete=models.CASCADE, blank=True)
 
     def __str__(self):
         return f'Artist: {self.artist} At: {self.venue} On: {self.show_date}'
@@ -59,11 +59,11 @@ class Show(models.Model):
 
 
 class Note(models.Model):
-    show = models.ForeignKey(Show, blank=False, on_delete=models.CASCADE)
-    user = models.ForeignKey('auth.User', blank=False, on_delete=models.CASCADE)
-    title = models.CharField(max_length=200, blank=False)
-    text = models.TextField(max_length=1000, blank=False)
-    posted_date = models.DateTimeField(auto_now_add=True, blank=False)
+    show = models.ForeignKey(Show, on_delete=models.CASCADE, blank=True)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, blank=True)
+    title = models.CharField(max_length=200, blank=True)
+    text = models.TextField(max_length=1000, blank=True)
+    posted_date = models.DateTimeField(auto_now_add=True, blank=True)
 
     def __str__(self):
         return f'User: {self.user} Show: {self.show} Note title: {self.title} Text: {self.text} Posted on: {self.posted_date}'
