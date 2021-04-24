d2e8cd8c5de961b693d7d8fd39c8d24844db4ae5
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-04-24 11:43:04 -0500

    Add bio to model

--- lmn/models.py
+++ lmn/models.py
@@ -82,6 +82,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # name = models.TextField(max_length=200, blank=False)
     twitter_username = models.TextField(max_length=15, blank=True)  # Twitter usernames cannot be longer than 15 characters
+    bio = models.TextField(max_length=2000, blank=True)
     favorite_artist = models.ForeignKey(Artist, blank=True, on_delete=models.SET_NULL)
     favorite_show = models.ForeignKey(Show, blank=True, on_delete=models.SET_NULL)
 
