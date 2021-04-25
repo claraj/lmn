63fbc1010677d10af18072278075bcca7e862263
Andrea <apratt1013@gmail.com>
2021-04-25 15:00:17 -0500

    remove unecessary db constraints

--- lmn/models.py
+++ lmn/models.py
@@ -72,9 +72,9 @@ class Badge(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_image = models.ImageField(upload_to='user_profile_images/', blank=True, null=True)
-    shows_seen = models.ManyToManyField(Show, blank=True)
+    shows_seen = models.ManyToManyField(Show)
     bio = models.TextField(blank=True, null=True)
-    badges = models.ManyToManyField(Badge, blank=True)
+    badges = models.ManyToManyField(Badge)
 
 
     def save(self, *args, **kwargs):
