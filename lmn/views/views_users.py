d3575acbffd7bc4f07e2129f026a90facff7a52a
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-05-08 17:39:55 -0500

    Import profile model and form

--- lmn/views/views_users.py
+++ lmn/views/views_users.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 
-from ..models import Venue, Artist, Note, Show
-from ..forms import VenueSearchForm, NewNoteForm, ArtistSearchForm, UserRegistrationForm
+from ..models import Venue, Artist, Note, Show, Profile
+from ..forms import VenueSearchForm, NewNoteForm, ArtistSearchForm, UserRegistrationForm, ProfileForm
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
