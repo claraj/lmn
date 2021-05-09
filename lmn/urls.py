b2e9ce59f2724da5c55b41adccaab09d6383f683
PickleEric <60155510+PickleEric@users.noreply.github.com>
2021-05-09 13:29:54 -0500

    Created path and template

--- lmn/urls.py
+++ lmn/urls.py
@@ -38,5 +38,6 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
     path('register/', views_users.register, name='register'),
+    path('goodbye_message/', views_users.logout, name="goodbye")
 
 ]
