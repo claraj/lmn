9e43bbed679628966f3cd3268ab2ee11a3b2958d
Clara <10088152+claraj@users.noreply.github.com>
2025-11-10 21:29:56 -0600

    headless mode, don't run tests if setup fails

--- lmn/functional_tests/test_home_page.py
+++ lmn/functional_tests/test_home_page.py
@@ -1,14 +1,22 @@
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.options import Options 
+import os 
 
+options = Options()
+
+# Run in headless mode, so don't actually display the browser window
+if 'GITHUB_ENV' in os.environ:
+    options.add_argument('--headless')
+# Else, running on a local computer, so show browser window. 
 
 class MySeleniumTests(StaticLiveServerTestCase):
 
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
-        cls.selenium = WebDriver()
+        cls.selenium = WebDriver(options=options)
         cls.selenium.implicitly_wait(10)
 
     @classmethod
